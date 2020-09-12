4f56d719897cd5d9224127a413f6ad795c95e36e
NikolaiK <kornienkokolya23@gmail.com>
2020-09-12 16:40:59 +0300

    Lesson2

---
+++ Client - Server/Assets/Scripts/NetUnitSetup.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class NetUnitSetup : NetworkBehaviour
+{
+    [SerializeField] MonoBehaviour[] _disableBihaviours;
+
+    private void Awake()
+    {
+        if (!hasAuthority)
+        {
+            for (int i = 0; i < _disableBihaviours.Length; i++)
+            {
+                _disableBihaviours[i].enabled = false;
+            }
+        }
+    }
+
+    public override void OnStartAuthority()
+    {
+        for (int i = 0; i < _disableBihaviours.Length; i++)
+        {
+            _disableBihaviours[i].enabled = true;
+        }
+    }
+}
